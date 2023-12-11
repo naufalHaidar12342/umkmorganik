f83836e30f43e149ada1f36252d6b9fb2ed9e49f
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2023-12-11 14:45:51 +0700

    add display="swap" to fonts

--- app/fonts.js
+++ app/fonts.js
@@ -3,8 +3,10 @@ import { Inter, Nunito } from "next/font/google";
 export const inter = Inter({
 	subsets: ["latin"],
 	weights: ["400", "500", "600", "700"],
+	display: "swap",
 });
 export const nunito = Nunito({
 	subsets: ["latin"],
 	weights: ["400", "500", "600", "700"],
+	display: "swap",
 });
